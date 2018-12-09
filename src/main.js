6288bb47e9b2f76e576a4c77de82dc8b92bc3a3a
Adrian <yohanesadrian97@gmail.com>
2018-12-09 18:36:16 +0700

    adding new get route with request params

--- src/main.js
+++ src/main.js
@@ -1,31 +1,69 @@
 //@flow
-type person = {
-  name :string,
-  age: number
-};
+import express from 'express';
+const app = express();
+const port = 3000;
 
-let josh: person = {
-  name:'josh',
-  age:2,
-}
-let dono: person = {
-  name:'dono',
-  age:2,
-}
-let rani: person = {
-  name:'rani',
-  age:2,
-}
-let a = 0;
-let people = [
-  josh,dono,rani
-]
-function getNames(people :Array<person>):Array<string>{
-  let names: Array<string> = [];
-  for (let person  of people) {
-    names.push(person.name);
+const dummyData = [
+  {
+    id: '1',
+    name: 'Acil',
+    umur: '90',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '2',
+    name: 'Acil2',
+    umur: '89',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '3',
+    name: 'Acil4',
+    umur: '67',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '4',
+    name: 'Acil5',
+    umur: '23',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '5',
+    name: 'Acil6',
+    umur: '54',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '6',
+    name: 'Acil7',
+    umur: '29',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '7',
+    name: 'Acil8',
+    umur: '29',
+    pekerjaan: 'pedagang',
+  },
+  {
+    id: '8',
+    name: 'Acil9',
+    umur: '89',
+    pekerjaan: 'pedagang',
+  },
+];
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/item/:id', (req, res) => {
+  console.log(req);
+  if (req.params && req.params.id && dummyData) {
+    for (let datum of dummyData) {
+      if (datum.id === req.params.id) {
+        res.send(datum);
+      }
+    }
   }
-  return names;
-}
+});
 
-console.log(getNames(people));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/main12.js
+++
@@ -1,6 +0,0 @@
-console.log(parseInt('1001', 2));
-
-5 , 2
-
-
-10  101
