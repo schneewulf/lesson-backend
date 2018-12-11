ad51a901d83947c2b104976b2d511e8d3b3d71da
Joshua <joshuasetiawan04@gmail.com>
2018-12-11 21:27:42 +0700

    move duckapil host to variable

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import express from 'express';
 import fetch from 'node-fetch';
 const app = express();
 const port = 3000;
+const dukcapilHost = process.env.DUKCAPIL_HOST || 'localhost:3000';
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/item/:id', async(req, res) => {
   console.log(req);
   if (req.params && req.params.id) {
-    let fetchPromise = await fetch(
-      `${process.env.DUKCAPIL_HOST || 'localhost:3000'}/item/${req.params.id}`
-    );
+    let fetchPromise = await fetch(`${dukcapilHost}/item/${req.params.id}`);
     let fetchResult = await fetchPromise.json();
     res.send(fetchResult);
   }
