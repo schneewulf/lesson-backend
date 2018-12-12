ee6ae38fda45ca74f43aff1060b25768befdb728
Joshua <joshuasetiawan04@gmail.com>
2018-12-12 11:42:55 +0700

    remove console log

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ const port = 3000;
 const dukcapilHost = process.env.DUKCAPIL_HOST || 'localhost:3000';
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/item/:id', async(req, res) => {
-  console.log(req);
   if (req.params && req.params.id) {
     let fetchPromise = await fetch(`${dukcapilHost}/item/${req.params.id}`);
     let fetchResult = await fetchPromise.json();
